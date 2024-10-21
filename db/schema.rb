2cd7c319a73491f2ef046c862c60643f5d4df9de
shimmen <y.shin3141@gmail.com>
2024-10-22 00:46:18 +0900

    add:回答機能の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_21_121522) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_21_151606) do
+  create_table "answers", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.integer "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
   create_table "questions", force: :cascade do |t|
     t.string "name"
     t.string "title"
@@ -18,4 +27,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_21_121522) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "answers", "questions"
 end
